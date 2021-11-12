17b72e89e59b4aeffd92151cdb6280dfcd9a3572
Raxipaxi <razielferundir@gmail.com>
2021-11-12 17:18:56 -0300

    Cameras Scripts

---
+++ Assets/Scripts/Props/Camera/SecurityCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCameraController : MonoBehaviour
+{
+
+    private SecurityCameraModel _cameraModel;
+    private PlayerModel _playerModel;
+
+    public event Action<Vector3> OnAlert;
+
+
+    public void OnAlertCommand(Vector3 dir)
+    {
+        OnAlert?.Invoke(dir);
+    }
+
+    private void Awake()
+    {
+        BakeReferences();
+    }
+
+    public void BakeReferences()
+    {
+        _cameraModel = GetComponent<SecurityCameraModel>();
+        _playerModel = GetComponent<PlayerModel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_cameraModel.LineOfSightAI.SingleTargetInSight(_playerModel.transform))
+        {
+            OnAlertCommand(_playerModel.transform.position);
+        }   
+    }
+}
---
+++ Assets/Scripts/Props/Camera/SecurityCameraModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCameraModel : MonoBehaviour
+{
+    private LineOfSightAI _lineOfSightAI;
+
+    public LineOfSightAI LineOfSightAI => _lineOfSightAI;
+    private void Awake()
+    {
+        BakeReferences();
+   }
+
+    public void BakeReferences()
+    {
+        _lineOfSightAI = GetComponent<LineOfSightAI>();
+    }
+}
